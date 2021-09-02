8a84fd8a16dafde27b46d58d4dc019d5fa70e2e9
kratere <23510328+kratere@users.noreply.github.com>
2021-09-02 08:12:14 +0200

    feat(home): update h1 + iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,6 @@ import * as React from 'react';
 import Nav from '../components/Nav/Nav';
 import Footer from '../components/Footer/Footer';
 
-// styles
-
-// data
-
-// markup
 const IndexPage = () => {
   return (
     <div>
@@ -15,18 +10,18 @@ const IndexPage = () => {
         <section>
           <title>Home Page</title>
           <h1>
-            Cabinet l'Effet Dentaire <br /> Votre chirurgien dentiste à
-            Périgueux et Boulazac.
+            Cabinet l'Effet Dentaire <br />
+            Votre équipe de chirurgien-dentiste à Périgueux et Boulazac.
           </h1>
           <div>
-            <p>Dr COSTA Charlotte</p>
-            <p>Dr DAOULAS Noémie</p>
-            <p>Dr TRIDAT Floriane</p>
+            <strong>Dr COSTA Charlotte</strong>
+            <strong>Dr DAOULAS Noémie</strong>
+            <strong>Dr TRIDAT Floriane</strong>
           </div>
         </section>
         <section>
           <h2>
-            Bienvenu sur le site web du Cabinet l'Effet Dentaire, votre centre
+            Bienvenue sur le site web du Cabinet l'Effet Dentaire, votre centre
             de soins dentaires à Périgueux et Boulazac.
           </h2>
           <article>
@@ -55,7 +50,7 @@ const IndexPage = () => {
         </section>
         <section>
           <h3>Nous trouver</h3>
-          <iframe></iframe>
+          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2814.225720107821!2d0.7475222155466871!3d45.14202377909847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ff7b8d5da8e4e3%3A0x1a0a012d0a03e64d!2sCabinet%20dentaire%20Dr%20Costa%2C%20Dr%20Daoulas%2C%20Dr%20Tridat!5e0!3m2!1sfr!2sfr!4v1630562750300!5m2!1sfr!2sfr'></iframe>
         </section>
       </main>
       <Footer />
