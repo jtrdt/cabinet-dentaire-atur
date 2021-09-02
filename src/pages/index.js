cb66d530a8a36a9490a1f6f4f275fb8d27fb256d
kratere <23510328+kratere@users.noreply.github.com>
2021-09-02 13:51:50 +0200

    chore(index): comment iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,10 @@ const IndexPage = () => {
         </section>
         <section>
           <h3>Nous trouver</h3>
-          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2814.225720107821!2d0.7475222155466871!3d45.14202377909847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ff7b8d5da8e4e3%3A0x1a0a012d0a03e64d!2sCabinet%20dentaire%20Dr%20Costa%2C%20Dr%20Daoulas%2C%20Dr%20Tridat!5e0!3m2!1sfr!2sfr!4v1630562750300!5m2!1sfr!2sfr'></iframe>
+          <iframe
+          // src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2814.225720107821!2d0.7475222155466871!3d45.14202377909847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ff7b8d5da8e4e3%3A0x1a0a012d0a03e64d!2sCabinet%20dentaire%20Dr%20Costa%2C%20Dr%20Daoulas%2C%20Dr%20Tridat!5e0!3m2!1sfr!2sfr!4v1630562750300!5m2!1sfr!2sfr'
+          // title='map'
+          ></iframe>
         </section>
       </main>
       <Footer />
