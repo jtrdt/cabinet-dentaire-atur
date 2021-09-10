6becdcd79d1ecd3650c7a7e1a38b2ffe9f4690e7
kratere <23510328+kratere@users.noreply.github.com>
2021-09-10 15:20:56 +0200

    build(metadata): add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.cabinet-dentaire-atur.fr`
+    siteUrl: `https://www.cabinet-dentaire-atur.fr`,
+    title: `Cabinet l'Effet Dentaire Atur Boulazac Isle Manoire Périgueux`,
+    description: `Site web du cabinet l'Effet Dentaire à Atur Boulazac Isle Manoire Périgueux`,
   },
-  plugins: []
+  plugins: [],
 };
